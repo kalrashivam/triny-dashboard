c63787d70b7bec890da47b8fd05c53efcf6d89c8
shivamengineersworkbook <shivamkalra2017@gmail.com>
2018-11-02 17:27:31 +0530

    mongoose setup complete

--- app.js
+++ app.js
@@ -6,13 +6,78 @@ var port = process.env.PORT || 3000;
 
 const express = require('express');
 const path = require("path");
-const bodyparser = require('body-parser');
+const bodyParser = require('body-parser');
 const request = require('request');
 const apiai = require('apiai');
 var hbs = require("handlebars");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const errorHandler = require("errorhandler");
+const session = require("express-session");
 
 const app = express();
 const apiaiapp = apiai("6ed416310a72408095ddd4bae851de2a");
+
+
+
+
+mongoose.promise = global.Promise;
+
+//Configure isProduction variable
+const isProduction = process.env.NODE_ENV === 'production';
+
+//Configure our app
+app.use(cors());
+app.use(require('morgan')('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({ secret: 'passport-tutorial', cookie: { maxAge: 60000 }, resave: false, saveUninitialized: false }));
+
+
+
+if (!isProduction) {
+    app.use(errorHandler());
+}
+
+//Configure Mongoose
+mongoose.connect('mongodb://localhost/passport-tutorial');
+mongoose.set('debug', true);
+
+// models and routes
+require("./models/Users");
+require("./config/passport");
+app.use(require("./routes"));
+
+//Error handlers & middlewares
+if (!isProduction) {
+    app.use((err, req, res, next) => {
+        res.status(err.status || 500);
+
+        res.json({
+            errors: {
+                message: err.message,
+                error: err,
+            },
+        });
+        next()
+    });
+}
+
+app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+
+    res.json({
+        errors: {
+            message: err.message,
+            error: {},
+        },
+    });
+    next()
+});
+
+
+
 
 app.listen(port, () => {
     console.log(`running on port no. ${port}`);
